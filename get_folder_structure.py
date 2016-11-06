3e43cb88f4049ca74ac53861328fdc0f8a0b8127
Eric <eric.kumlin@gmail.com>
2016-11-05 20:45:43 -0700

    Add PY file to get folder structure of a dir

---
+++ get_folder_structure.py
@@ -0,0 +1,28 @@
+import os
+import re
+import datetime
+
+rootDir = 'C:\\Windows'
+
+def printDirs(rootDir, depth = 0):
+	rootLength = len(re.split('[\\\\/]', rootDir));
+
+	for root, dirs, files in os.walk(rootDir):
+		path = re.split('[\\\\/]', root)
+		pathLength = len(path) - rootLength + 1
+
+		print (depth + pathLength - 1) * '\t', os.path.basename(root)
+
+		for fname in files:
+			fullPath = os.path.join(root, fname)
+
+			try:
+				(mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(fullPath)
+				modTime = datetime.datetime.fromtimestamp(mtime).strftime("%Y/%m/%d %H:%M:%S")
+			except:
+				size = 0
+				modTime = datetime.datetime.fromtimestamp(0).strftime("%Y/%m/%d %H:%M:%S")
+
+			print (depth + pathLength) * '\t', '%s\t\t%d\t%s' % (fname, size, modTime)
+
+printDirs(rootDir)
